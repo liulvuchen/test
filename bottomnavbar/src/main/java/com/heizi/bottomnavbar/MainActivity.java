ed953031fda925819176956aa6c8656e41647307
liuyaolin@zjipst.com <Lyl8341431>
2021-04-21 15:35:40 +0800

    init

---
+++ bottomnavbar/src/main/java/com/heizi/bottomnavbar/MainActivity.java
@@ -0,0 +1,23 @@
+package com.heizi.bottomnavbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RadioButton rbTab1, rbTab2, rbTab3, rbTab4;
+    private RadioGroup layTabBottom;
+    private List<Fragment> mBaseFragment; //集合
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
